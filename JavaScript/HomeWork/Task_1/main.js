05739f890efe76334a44e672a77fa3c12056d0b6
KOJIOTOB <htc.kbc@gmail.com>
2022-04-16 22:42:27 +0300

    as

---
+++ JavaScript/HomeWork/Task_1/main.js
@@ -0,0 +1,182 @@
+/*
+- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+    Вивести кожну змінну за допомогою: console.log , alert, document.write
+*/
+
+/*
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+*/
+
+/*
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+*/
+
+/*
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(i);
+alert(j);
+alert(k);
+alert(l);
+alert(m);
+*/
+
+/*
+document.write(a);
+document.write(`<br>`)
+document.write(b);
+document.write(`<br>`)
+document.write(c);
+document.write(`<br>`)
+document.write(d);
+document.write(`<br>`)
+document.write(e);
+document.write(`<br>`)
+document.write(f);
+document.write(`<br>`)
+document.write(g);
+document.write(`<br>`)
+document.write(h);
+document.write(`<br>`)
+document.write(i);
+document.write(`<br>`)
+document.write(k);
+document.write(`<br>`)
+document.write(j);
+document.write(`<br>`)
+document.write(l);
+document.write(`<br>`)
+document.write(m);
+*/
+
+
+/*
+- Створити об'єкт book з наступними полями :
+назва, (тип string)
+кількість сторінок (числовий тип),
+жанр (string)
+*/
+
+/*let book = {
+    title: 'Чорнобаївка',
+    pages: 15,
+    genre: 'бермудський трикутник',
+}
+console.log(book)
+console.log(book.title)*/
+
+
+/*- Створити об'єкт book з наступними полями :
+назва, (тип string)
+кількість сторінок (числовий тип),
+жанр (string)
+автори (тип - масив, кожен елемент масиву - це стрінга)*/
+
+
+/*
+let book2 = {
+    title: 'русский корабль',
+    pages: 8,
+    genre: 'олени',
+    author: [
+        {name: 'володя'},
+        {name: 'шойгу'},
+    ]
+}
+console.log(book2)
+console.log(book2.author)
+console.log(book2.author.length)
+console.log(book2.author[0])
+*/
+
+
+/*- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+    З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)*/
+
+/*let lastName = 'Колотов';
+let firstName = 'Вiктор';
+let middleName = 'Сергiйович';*/
+
+// let person = lastName + ' ' + firstName + ' ' + middleName;
+/*let person = `${lastName} ${firstName} ${middleName}`;
+
+console.log(person);*/
+
+
+/*
+- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям,
+ По-Батькові та роками. та вивести в консоль
+*/
+
+/*
+let lastName = prompt('Введiть Ваше Iм`я!!!');
+let middleName = prompt('Введiть Ваше Iм`я По-Батькові!!!');
+let age = prompt('Скiльки Вам рокiв було вчора???');
+
+console.log(lastName);
+console.log(middleName);
+console.log(age);
+*/
+
+
+/*
+- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+    let a = 100; let b = '100'; let c = true;*/
+
+/*
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+*/
+
+/*
+- Створити масив об'єктів з назвою books з наступними полями
+- назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+*/
+
+/*
+let books = [
+    {title: 'java', pages: 300, genre: 'action', author: 'Jon Doe'},
+    {title: 'php', pages: 280, genre: 'history', author: 'David Script'},
+    {title: 'python', pages: 500, genre: 'work', author: 'Fedor'}
+];
+
+console.log(books);
+console.log(books[0]);
+console.log(books[1]);
+console.log(books[2]);
+*/
